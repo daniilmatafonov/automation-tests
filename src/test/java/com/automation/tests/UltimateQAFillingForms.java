1225106234b57c074ed4011861274368f60502bf
Daniil Matafonov <dmatafonov@wsoft.ru>
2019-04-06 18:56:31 +0300

    fix imports

--- src/test/java/com/automation/tests/UltimateQAFillingForms.java
+++ src/test/java/com/automation/tests/UltimateQAFillingForms.java
@@ -1,10 +1,10 @@
 package com.automation.tests;
 
 import org.junit.Test;
-import com.codeborne.selenide.CollectionCondition;
 import org.openqa.selenium.By;
-
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class UltimateQAFillingForms {
 
@@ -16,8 +16,7 @@ public class UltimateQAFillingForms {
         $(By.id("et_pb_contact_name_0")).val("User");
         $(By.cssSelector("#et_pb_contact_message_0")).val("Hello");
         $(By.cssSelector("#et_pb_contact_form_0 > div.et_pb_contact > form > div > button")).submit();
-        $$(By.cssSelector("#et_pb_contact_form_0 > div > p")).shouldHave(CollectionCondition.texts(("Form filled out successfully")));
-
+        $(By.cssSelector("#et_pb_contact_form_0 > div > p")).shouldHave(text(("Form filled out successfully")));
     }
 
 
